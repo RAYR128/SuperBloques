78391d7da3d0ba1fb566e6bd61157a817310fd28
PC <example@example.com>
2026-09-01 16:50:28 -0400

    organizacion: mover scripts cmake a una carpeta dedicada.

--- dev_win.bat
+++ dev_win.bat
@@ -1,4 +1,4 @@
-cmake -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=windows.cm -DCMAKE_BUILD_TYPE=Release . -B build-windows
+cmake -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=cmake/windows.cm -DCMAKE_BUILD_TYPE=Release . -B build-windows
 cd build-windows
 make -j8
 pause
